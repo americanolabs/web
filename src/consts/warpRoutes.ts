f23c91562c0af59f1b3720e125c4d5260f5e0609
FjrREPO <jossfajar21@gmail.com>
2025-03-26 15:13:53 +0700

    Update warpRoutes configuration: modify ROUTER address and streamline token connections

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,22 +1,12 @@
 import { type WarpCoreConfig } from '@hyperlane-xyz/sdk';
 import { zeroAddress } from 'viem';
 
-const ROUTER = '0xf614c6bF94b022E16BEF7dBecF7614FFD2b201d3';
-const ROUTER_DECAF = '0xd07A45dD047c67662F9877acd60902A4EE0aB847';
+const ROUTER = '0xC1d0d7C961daF74BECa71416359a92c59c8A8012';
 
 const NETWORK_SEPARATOR = '101010';
 
 export const TOP_MAX = {
-  'bsesepolia': {
-    [zeroAddress]: 1e16,
-  },
-  'optimismsepolia': {
-    [zeroAddress]: 1e16,
-  },
   'arbitrumsepolia': {
-    [zeroAddress]: 1e16,
-  },
-  'sepolia': {
     [zeroAddress]: 1e16,
   },
   'decaftestnet': {
@@ -31,62 +21,9 @@ export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     {
       addressOrDenom: zeroAddress,
-      chainName: 'optimismsepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|basesepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|sepolia|' + zeroAddress,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ETH',
-      standard: 'IntentNative',
-      symbol: 'ETH',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: zeroAddress,
-      chainName: 'basesepolia',
-      collateralAddressOrDenom: "0xf614c6bF94b022E16BEF7dBecF7614FFD2b201d3",
-      connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|sepolia|' + zeroAddress,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ETH',
-      standard: 'IntentNative',
-      symbol: 'ETH',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: zeroAddress,
       chainName: 'arbitrumsepolia',
       collateralAddressOrDenom: ROUTER,
       connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|basesepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|sepolia|' + zeroAddress,
-        },
         {
           token: 'ethereum|decaftestnet|' + zeroAddress,
         }
@@ -100,30 +37,8 @@ export const warpRouteConfigs: WarpCoreConfig = {
     },
     {
       addressOrDenom: zeroAddress,
-      chainName: 'sepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|basesepolia|' + zeroAddress,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ETH',
-      standard: 'IntentNative',
-      symbol: 'ETH',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: zeroAddress,
       chainName: 'decaftestnet',
-      collateralAddressOrDenom: ROUTER_DECAF,
+      collateralAddressOrDenom: ROUTER,
       connections: [
         {
           token: 'ethereum|arbitrumsepolia|' + zeroAddress,
@@ -140,17 +55,11 @@ export const warpRouteConfigs: WarpCoreConfig = {
   options: {
     interchainFeeConstants: [
       {
-        amount: 3e14,
-        origin: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia', 'decaftestnet'].join(NETWORK_SEPARATOR),
-        destination: 'sepolia',
-        addressOrDenom: zeroAddress,
-      },
-      {
         amount: 1e10,
-        origin: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia', 'sepolia', 'decaftestnet'].join(
+        origin: ['arbitrumsepolia', 'decaftestnet'].join(
           NETWORK_SEPARATOR,
         ),
-        destination: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia', 'decaftestnet'].join(NETWORK_SEPARATOR),
+        destination: ['arbitrumsepolia', 'decaftestnet'].join(NETWORK_SEPARATOR),
         addressOrDenom: zeroAddress,
       },
     ],
